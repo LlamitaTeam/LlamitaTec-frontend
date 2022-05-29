5beea37d024f1b2f107cd2bc340a83298f092638
FrancoVasquez <u201920339@upc.edu.pe>
2022-05-28 22:28:09 -0500

    feat: service for register page implemented

--- src/app/register/services/register.service.ts
+++ src/app/register/services/register.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {catchError, Observable, retry, throwError} from "rxjs";
-import {User} from "../model/user";
 
 @Injectable({
   providedIn: 'root'
