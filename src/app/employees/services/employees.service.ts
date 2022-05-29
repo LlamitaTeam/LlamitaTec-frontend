d01c97c5c8a71f06e49f51ab274c0b3ce1cb009f
piers5001 <64555167+piers5001@users.noreply.github.com>
2022-05-28 23:01:13 -0500

    feat: employee service

---
+++ src/app/employees/services/employees.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {catchError, Observable, retry, throwError} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeesService {
+
+  basePath = 'http://localhost:3000/employees';
+
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if(error.error instanceof ErrorEvent) {
+      console.log(`Ann error occurred: ${error.error.message}`);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was: ${error.error}`
+      );
+    }
+    return throwError('Something happened with request, please try again later');
+  }
+
+  getAll() {
+    return this.http.get(this.basePath, this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  getById(id: any) {
+    return this.http.get(`http://localhost:3000/services/${id}/employees`, this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+}
